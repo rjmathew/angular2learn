0e8cc6e1462f434e5081f3c1c23bb920accc33e3
rajesh <rjmathew@gmali.com>
2016-10-03 22:12:19 -0700

    child component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
+import {CustomerComponent} from "./customer/customer.component";
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent , CustomerComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
