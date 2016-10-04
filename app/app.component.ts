1e8d905de402bb2f079ced37799aaa299349b645
rajesh <rjmathew@gmali.com>
2016-10-03 21:50:17 -0700

    ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html'
@@ -7,8 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Rajesh';
+  title = 'First Angular 2 App';
   testColor = 'green';
+  name='bob';
+  customers=[
+    {id:1, name:'aleena'},{
+      id:2, name:'sophie'
+    },{
+      id:3, name:'rajesh'
+    }
+  ];
   changeColor() {
     if (this.testColor=='red')
       this.testColor='green';
